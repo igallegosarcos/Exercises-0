c124cdc09b8ec604a18485083fa95e769ca687d2
Gallegos Arcos <igallego@t-systems.es>
2022-11-11 17:38:55 +0100

    Format project

--- src/main/java/com/curso/iniciacion/RelacionCero.java
+++ src/main/java/com/curso/iniciacion/RelacionCero.java
@@ -9,106 +9,101 @@ package com.curso.iniciacion;
  * @author igallego
  */
 public class RelacionCero {
-    
-    public static boolean isPalindrome(String word){
-        word=word.toLowerCase();
-        int i=0;
-        boolean isPalindrome=true;    
-            while (i<word.length()/2 && isPalindrome){
-                if (word.charAt(i)!= word.charAt(word.length()-i-1))
-                  isPalindrome=false;
-                else 
-                   i++;
-        } 
+
+    public static boolean isPalindrome(String word) {
+        word = word.toLowerCase();
+        int i = 0;
+        boolean isPalindrome = true;
+        while (i < word.length() / 2 && isPalindrome) {
+            if (word.charAt(i) != word.charAt(word.length() - i - 1)) {
+                isPalindrome = false;
+            } else {
+                i++;
+            }
+        }
         return isPalindrome;
     }
-    
-    public static boolean isPalindrome (int number){
-        int invert=0;
-        boolean isPalindrome=true;
-        while (number > 0){
-            int right=number%10;
-            invert=invert*10+right;
-            number=(number-right)/10;          
-        }    
-            if(number!=invert){
-                
-                isPalindrome=false;
-            }
-            return isPalindrome;
-    
+
+    public static boolean isPalindrome(int number) {
+        int invert = 0;
+        boolean isPalindrome = true;
+        while (number > 0) {
+            int right = number % 10;
+            invert = invert * 10 + right;
+            number = (number - right) / 10;
+        }
+        if (number != invert) {
+
+            isPalindrome = false;
+        }
+        return isPalindrome;
+
     }
-    
-    public static int getFactorial (int number){
-        int factorial=1;
-        while (number >0){
-            factorial=number*factorial;
+
+    public static int getFactorial(int number) {
+        int factorial = 1;
+        while (number > 0) {
+            factorial = number * factorial;
             number--;
         }
         return factorial;
     }
-    
-   public static int getMcd (int n1, int n2){
-      while (n2!=0){
-          int mcd=n2;
-          n2 = n1%n2;
-          n1=mcd;
-      }
-  return n1;   
-   }
-   
-   public static boolean isArmstrong (int number){
-       int count=0;
-       int numbertwo=number;
-       if (number==0){
-           count=1;
-       } else{
-           for( int i=number ; i>0; i/= 10){
-               count++;
-           }
-       }
-        boolean isArmstrong=true;
-        double right;
-        double armstrong=0;
-        while (number > 0){
-            right=number%10;
-            number /=10;
-            armstrong += Math.pow(right, count);          
-        }    
-            if(numbertwo!=armstrong){
-                
-                isArmstrong=false;
-            }
-            return isArmstrong; 
-   }
-   
-   public static String reverse (String phrase){
-    int i=0;
-    String change="";
-    while (i<phrase.length()){
-        change += phrase.charAt(phrase.length()-i-1);
-        i++;
-    }
-    return change;
-   }
-   
-    public static void rice (float number){
-        int box5 = (int)number/5;
-        System.out.println("Paquetes de 5kg: "+box5);
-        int rest = (int) number%5;
-        if (rest!=0){
-            System.out.println("Paquetes de 3kg: "+rest/3);
+
+    public static int getMcd(int n1, int n2) {
+        while (n2 != 0) {
+            int mcd = n2;
+            n2 = n1 % n2;
+            n1 = mcd;
         }
-        rest%=3;
-        if(rest!=0){
-            System.out.println("Paquetes de 1kg: " +rest);
-        } 
-   }
-   
-   
-   
-   
-   
-   
-   
+        return n1;
+    }
+
+    public static boolean isArmstrong(int number) {
+        int count = 0;
+        int numbertwo = number;
+        if (number == 0) {
+            count = 1;
+        } else {
+            for (int i = number; i > 0; i /= 10) {
+                count++;
+            }
+        }
+        boolean isArmstrong = true;
+        double right;
+        double armstrong = 0;
+        while (number > 0) {
+            right = number % 10;
+            number /= 10;
+            armstrong += Math.pow(right, count);
+        }
+        if (numbertwo != armstrong) {
+
+            isArmstrong = false;
+        }
+        return isArmstrong;
+    }
+
+    public static String reverse(String phrase) {
+        int i = 0;
+        String change = "";
+        while (i < phrase.length()) {
+            change += phrase.charAt(phrase.length() - i - 1);
+            i++;
+        }
+        return change;
+    }
+
+    public static void rice(float number) {
+        int box5 = (int) number / 5;
+        System.out.println("Paquetes de 5kg: " + box5);
+        int rest = (int) number % 5;
+        if (rest != 0) {
+            System.out.println("Paquetes de 3kg: " + rest / 3);
+        }
+        rest %= 3;
+        if (rest != 0) {
+            System.out.println("Paquetes de 1kg: " + rest);
+        }
+    }
+
 }
