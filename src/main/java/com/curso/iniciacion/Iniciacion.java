1ae49929abe183cf1851beef4499785528c6ab7e
Gallegos Arcos <igallego@t-systems.es>
2022-11-11 17:32:04 +0100

    Upload exercises 0

---
+++ src/main/java/com/curso/iniciacion/Iniciacion.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.curso.iniciacion;
+
+/**
+ *
+ * @author igallego
+ */
+public class Iniciacion {
+
+    public static void main(String[] args) {
+        //Ejercicio 1
+        //System.out.println(RelacionCero.isPalindrome("Ana"));
+        //System.out.println(RelacionCero.isPalindrome("Antonio"));
+        //Ejercicio 2
+        //System.out.println(RelacionCero.isPalindrome("1221"));
+        //System.out.println(RelacionCero.isPalindrome("1234"));
+        //Ejercicio 3
+        //System.out.println(RelacionCero.getFactorial(6));
+        //Ejercicio 4
+        //System.out.println(RelacionCero.getMcd(48, 60));
+        //Ejercicio 5
+        //System.out.println(RelacionCero.isArmstrong(12));
+        //Ejercicio 6
+        //System.out.println(RelacionCero.reverse("Chocolate caliente"));
+        // Ejercicio 7
+        //RelacionCero.rice((float) 23.5);
+        
+        
+        
+    }
+    
+    
+    
+}
